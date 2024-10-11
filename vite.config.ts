47bcf0ec8b0afb0f13eed86cd2afec4a6f7934dd
Devika S <devika.s@hubspire.com>
2024-10-11 14:53:01 +0530

    initial upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Covid-Tracker-App/",
   css: {
     postcss: './postcss.config.js',
   },
